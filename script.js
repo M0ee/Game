e2fa20a9de1b855a3e30425d2ff95923f5b07ecb
M0ee <abu.kasm96@gmail.com>
2022-04-29 14:18:27 +0200

    initial commit

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
 var character = document.getElementById("character");
 var block = document.getElementById("block");
+var counter=0;
 
 function jump(){
 
     character.classList.add("animate");
     setTimeout(function(){
         character.classList.remove("animate");
+
     },600);
 }
 
 function Move(){
     if(block.classList == "animate1"){return}
     block.classList.add("animate1");
+    setInterval(checkScore, 25);
+
 }
+
 
 var checkDead = setInterval(function() {
     let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
@@ -22,3 +27,6 @@ var checkDead = setInterval(function() {
         block.classList.remove("animate1");
     }
 }, );
+
+
+
