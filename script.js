3144b4805a19a247d713e9b400ea536f9c72ef3f
M0ee <abu.kasm96@gmail.com>
2022-04-29 11:57:21 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+var character = document.getElementById("character");
+var block = document.getElementById("block");
+
+function jump(){
+
+    character.classList.add("animate");
+    setTimeout(function(){
+        character.classList.remove("animate");
+    },600);
+}
+
+function Move(){
+    if(block.classList == "animate1"){return}
+    block.classList.add("animate1");
+}
+
+var checkDead = setInterval(function() {
+    let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    if(blockLeft<120 && blockLeft>80 && characterTop>=130){
+        alert("Game Over.");
+        block.classList.remove("animate1");
+    }
+}, );
